c645667a4b8d5e1d681fe0067d6bd6d1c71d1dba
Aprajita <mishraaprajita43@gmail.com>
2026-01-16 12:44:07 +0530

    update day2

---
+++ JS day-2.js
@@ -0,0 +1,38 @@
+let number =[1,2,3];
+let dounle = number.map (x=>x*2);
+condole.log (doubled);
+
+
+//map method 
+// let numbers = [1,2,3];
+// let doubled = number.map((aprajita)=>aprajita*2);
+// console.log (doubled);
+
+//filter method 
+// let marks = [7,23,24,11,15,29,4,0];
+// let result = marks.filter ((mark)=>mark>=16);
+// console.log (result);
+
+
+
+//q.1 take an. array with 10 different marks. and print all the marks and message 
+// of pass and . eg: "pass",12 "fail "
+// let marks = [7,23,34,11,15,29,16,37];
+// let result = marks.filter ((mark)=>){
+
+
+// }
+
+
+
+
+//q.2 find the sum of all the elements inside an array using for loop?
+/*let arr = [10, 20, 30, 40, 50];
+let sum = 0;
+
+for (let i=0; i<arr.length;i++){
+    sum+=arr[i]
+
+}*/
+// let num=[1,2,3,4]
+// console.log(num)
