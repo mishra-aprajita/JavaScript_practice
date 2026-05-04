f9642c6f0d025846ca0f60501a05733f75e97276
Aprajita <mishraaprajita43@gmail.com>
2026-05-04 23:09:44 +0530

    js

--- Mid-term2/script.js
+++ Mid-term2/script.js
@@ -1,4 +1,4 @@
-console.log("A");
+/*console.log("A");
 setTimeout(()=>{
   console.log("B");
 },2000);
@@ -6,26 +6,26 @@ setTimeout(()=>{
   console.log("c");
 },5000);
 
-console.log("D");
+console.log("D");*/
 
 
 
 // document.getElementById("demo").innerHTML = "Hi";
 // document.getElementById("hello").innerHTML="UO";
 // 1. innerHTML
-//<p id="demo"><b>Hello</b> World</p>
-//document.getElementById("demo").innerHTML
+// <p id="demo"><b>Hello</b> World</p>
+// document.getElementById("demo").innerHTML
 // 2. innerText
-//<p id="demo"><b>Hello</b> World</p>
-//document.getElementById("demo").innerText
+// <p id="demo"><b>Hello</b> World</p>
+// document.getElementById("demo").innerText
 // 3. textContent
-//document.getElementById("demo").textContent 
+// document.getElementById("demo").textContent 
 // function A(){
 //  console.log("A");
 // }
 
-// function B(){
-//  A();
-// }
+function B(){
+ A();
+}
 
-// B();
+B();
